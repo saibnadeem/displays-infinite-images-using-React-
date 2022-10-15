d9fcdbfbc9e35f38dae8c5dae1bd06a0f3bf99a3
Saqib Nadeem <76222155+saibnadeem@users.noreply.github.com>
2022-10-15 12:49:56 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# displays-infinite-images-using-React
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import InputForm from "../InputForm/InputForm";
+import "./search-bar.css";
+
+class SearchBar extends Component {
+  render() {
+    return <InputForm />;
+  }
+}
+export default SearchBar;
